d8badcb86eb47131229adc61d078d8d9170aa52a
freebits <freebits@users.noreply.github.com>
2022-06-10 11:37:15 +1000

    update

--- config/config.php
+++ config/config.php
@@ -2,10 +2,10 @@
 
 function get_config() {
     return array(
-        'MAILGUN_KEY' => 'key-7a9bb86b317120698eecae4cf3e52143',
-        'MAIL_FROM' => 'noreply@86400.global',
+        'MAIL_KEY' => 'key-7a9bb86b317120698eecae4cf3e52143',
+        'MAIL_FROM' => 'noreply@mail.86400.global',
         'MAIL_TO' => 'freebits1@gmail.com',
-        'MAILGUN_DOMAIN' => 'mail.86400.global',
+        'MAIL_DOMAIN' => 'mail.web997.com',
         'DB_NAME' => '86400.global',
         'DB_USER' => 'trader'
     );
