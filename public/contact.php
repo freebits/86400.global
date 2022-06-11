0ac16c45677be68acd3ab42e88adc1359d8e2d1a
freebits <freebits@users.noreply.github.com>
2022-06-12 05:57:15 +1000

    updated formatting

--- public/contact.php
+++ public/contact.php
@@ -1,6 +1,12 @@
 <?php
 require_once dirname(__DIR__).'/vendor/autoload.php';
 
+
+define('MAX_NAME_LENGTH', 128)
+define('MAX_PHONE_LENGTH', 32)
+define('MAX_EMAIL_LENGTH', 128)
+define('MAX_MESSAGE_LENGTH', 2048)
+
 $name = '';
 $name_error = '';
 $phone = '';
@@ -18,26 +24,26 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     if (strlen($name) === 0) {
         $name_error = 'Name is required.';
-    } elseif (strlen($name) > 128) {
-        $name_error = 'Name must be less than 128 characters.';
+    } elseif (strlen($name) > NAME_MAX_LENGTH) {
+        $name_error = 'Name must be less than '.NAME_MAX_LENGTH.' characters.';
     }
 
     if (strlen($phone) === 0) {
         $phone_error = 'Phone is required.';
-    } elseif (strlen($phone) > 32) {
-        $phone_error = 'Phone must be less than 32 characters.';
+    } elseif (strlen($phone) > PHONE_MAX_LENGTH) {
+        $phone_error = 'Phone must be less than '.PHONE_MAX_LENGTH.' characters.';
     }
 
     if (strlen($email) === 0) {
         $email_error = 'Email is required.';
-    } elseif (strlen($email) > 128) {
-        $email_error = 'Email must be less than 128 characters.';
+    } elseif (strlen($email) > EMAIL_MAX_LENGTH) {
+        $email_error = 'Email must be less than '.EMAIL_MAX_LENGTH.' characters.';
     }
 
     if (strlen($message) === 0) {
         $message_error = 'Message is required.';
-    } elseif (strlen($message) > 2048) {
-        $message_error = 'Message must be less than 2048 characters.';
+    } elseif (strlen($message) > MESSAGE_MAX_LENGTH) {
+        $message_error = 'Message must be less than '.MESSAGE_MAX_LENGTH.' characters.';
     }
 
     if (empty($name_error) &&
