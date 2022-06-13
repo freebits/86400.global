97d7b88f19017bc102b4786f53b06d2c0aa46853
abc def <radicaldata@users.noreply.github.com>
2022-06-13 10:12:36 +1000

    Removed CDNs and added local assets

--- views/contact_template.php
+++ views/contact_template.php
@@ -3,13 +3,11 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">    
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css">
-        <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
-        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.css"/>
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <!--<link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;900&display=swap" rel="stylesheet"> -->
+        <link href="/lib/css/bootstrap.min.css" rel="stylesheet"/>
+        <link rel="stylesheet" href="/lib/css/bootstrap-icons.css">
+        <link rel="stylesheet" type="text/css" href="/lib/css/slick.css"/>
+        <link rel="stylesheet" type="text/css" href="/lib/css/slick-theme.css"/>
+        <link rel="stylesheet" href="/lib/css/style.css">
         <link rel="stylesheet" href="/lib/css/style.css">
         <title>Contact Us| 86400.global</title>
         <meta name="description" content="Premium Web Design based in Brisbane, Queensland, Australia.
