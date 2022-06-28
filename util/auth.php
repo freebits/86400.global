f7a1159463bf16c292d74ca0d8fe6d08374b6765
freebits <freebits@users.noreply.github.com>
2022-06-29 08:35:26 +1000

    update

--- util/auth.php
+++ util/auth.php
@@ -5,3 +5,9 @@ function check_password(string $username, string $password): boolean
     $password_hash = get_password_hash($username);
     return password_verify($password, $password_hash);
 }
+
+
+function generate_password_hash(string $clear_text_password): string
+{
+    return password_hash($clear_text_password, PASSWORD_DEFAULT);
+}
