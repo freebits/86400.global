31421d200d0aed3246f10b27084fdfe4535e128b
freebits <freebits@users.noreply.github.com>
2022-07-17 14:39:05 +1000

    update

---
+++ public/subscriptions.php
@@ -0,0 +1,9 @@
+<?php
+require_once dirname(__DIR__).'/vendor/autoload.php';
+
+$loader = new \Twig\Loader\FilesystemLoader('../views/');
+$twig = new \Twig\Environment($loader, []);
+
+$subscriptions = get_subscriptions();
+
+echo $twig->render('subscriptions_template.twig', ['subscriptions' => $subscriptions ]);
