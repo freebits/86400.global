de2e83f48877d8eb6476d29e653b8be7c8453934
freebits <freebits@users.noreply.github.com>
2022-07-20 09:31:28 +1000

    update

---
+++ models/charge_db.php
@@ -0,0 +1,15 @@
+<?php
+require dirname(__DIR__).'/vendor/autoload.php';
+
+function add_charge(int $account_subscription_id): void
+{
+    $dbh = get_database();
+    $sth = $dbh->prepare(
+        'INSERT INTO charge (account_subscription_id)
+        VALUES (:account_subscription_id)'
+    );
+    $sth->bindParam(':account_subscription_id', $name, PDO::PARAM_INT);
+    $sth->execute();
+    $dbh = null;
+    return;
+}
