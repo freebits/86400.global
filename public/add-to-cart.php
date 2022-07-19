7d1d18cfc0be1e2b4d5f0cdd8c8e926ad5741343
freebits <freebits@users.noreply.github.com>
2022-07-19 15:52:57 +1000

    update

--- public/add-to-cart.php
+++ public/add-to-cart.php
@@ -1,62 +1,41 @@
 <?php
 require_once dirname(__DIR__).'/vendor/autoload.php';
 
-define('MAX_CREDIT_CARD_NUMBER_LENGTH', 128);
-define('MAX_EXPIRARY_MONTH_LENGTH', 32);
-define('MAX_EXPIRARY_YEAR_LENGTH', 32);
-define('MAX_CVV_LENGTH', 32);
+define('MAX_SUBSCRIPTION_ID_LENGTH', 128);
+define('MAX_QUANTITY_LENGTH', 32);
 
 $loader = new \Twig\Loader\FilesystemLoader('../views/');
 $twig = new \Twig\Environment($loader, []);
 
-$credit_card_number = '';
-$expirary_month = '';
-$expirary_year = '';
-$cvv = '';
+$subscription_id = '';
+$quantity = '';
 
-$credit_card_number_error = '';
-$expirary_month_error = '';
-$expirary_year_error = '';
-$cvv_error = '';
-
+$subscription_id_error = '';
+$quantity_error = '';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $credit_card_number = filter_var($_POST['credit_card_number'], FILTER_SANITIZE_STRING);
-    $expirary_month = filter_var($_POST['expirary_month'], FILTER_SANITIZE_STRING);
-    $expirary_year = filter_var($_POST['expirary_year'], FILTER_SANITIZE_STRING);
-    $cvv = filter_var($_POST['cvv'], FILTER_SANITIZE_STRING);
+    $subscription_id = filter_var($_POST['subscription_id'], FILTER_SANITIZE_STRING);
+    $quantity = filter_var($_POST['quantity'], FILTER_SANITIZE_STRING);
     
-    if (strlen($credit_card_number) === 0) {
-        $credit_card_number_error = 'Credit card number is required.';
-    } elseif (strlen($credit_card_number) > MAX_CREDIT_CARD_NUMBER_LENGTH) {
-        $credit_card_number_error = 'Credit card number must be less than '.MAX_CREDIT_CARD_NUMBER_LENGTH.' characters.';
+    if (strlen($subscription_id) === 0) {
+        $subscription_id_error = 'Credit card number is required.';
+    } elseif (strlen($subscription_id) > MAX_SUBSCRIPTION_ID_LENGTH) {
+        $subscription_id_error = 'Credit card number must be less than '.MAX_SUBSCRIPTION_ID_LENGTH.' characters.';
     }
 
-    if (strlen($expirary_month) === 0) {
-        $expirary_month_error = 'Expirary month is required.';
-    } elseif (strlen($expirary_month) > MAX_EXPIRARY_MONTH_LENGTH) {
-        $expirary_month_error = 'Expirary month must be less than '.MAX_EXPIRARY_MONTH_LENGTH.' characters.';
+    if (strlen($quantity) === 0) {
+        $quantity_error = 'Expirary month is required.';
+    } elseif (strlen($quantity) > MAX_QUANTITY_LENGTH) {
+        $quantity_error = 'Expirary month must be less than '.MAX_QUANTITY_LENGTH.' characters.';
     }
 
-    if (strlen($expirary_year) === 0) {
-        $expirary_year_error = 'Expirary year is required.';
-    } elseif (strlen($expirary_year) > MAX_EXPIRARY_YEAR_LENGTH) {
-        $expirary_year_error = 'Expirary year must be less than '.MAX_EXPIRARY_YEAR_LENGTH.' characters.';
-    }
-
-    if (strlen($cvv) === 0) {
-        $cvv_error = 'CVV is required.';
-    } elseif (strlen($cvv) > MAX_CVV_LENGTH) {
-        $cvv_error = 'CVV must be less than '.MAX_CVV_LENGTH.' characters.';
-    }
-
-    if (empty($credit_card_number_error) && empty($expirary_month_error) && empty($expirary_year_error) && empty($cvv_error)) {
+    if (empty($subscription_id_error) && empty($quantity_error)) {
         header('Location: /success.html');
     }
 }
 
-echo $twig->render('add_credit_card_template.twig', ['credit_card_number_error' => $credit_card_number_error,
-                                             'expirary_month_error' => $expirary_month_error,
+echo $twig->render('add_credit_card_template.twig', ['subscription_id_error' => $subscription_id_error,
+                                             'quantity_error' => $quantity_error,
                                              'expirary_year_error' => $expirary_year_error,
                                              'cvv_error' => $cvv_error
                                             ]);
