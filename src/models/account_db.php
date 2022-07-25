b8ba31c8296f3cfe7f548feaba422f67825c5be7
freebits <freebits@users.noreply.github.com>
2022-07-26 06:52:08 +1000

    update

--- src/models/account_db.php
+++ src/models/account_db.php
@@ -1,19 +1,21 @@
 <?php
 require_once __DIR__.'/../../vendor/autoload.php';
 
-function add_account(string $username, string $password_hash, string $stripe_customer_id): void
+function add_account(string $username, string $password_hash, string $stripe_customer_id): int
 {
     $dbh = get_database();
     $sth = $dbh->prepare(
         'INSERT INTO account(username, password_hash, stripe_customer_id)
-        VALUES (:username, :password_hash, :stripe_customer_id)'
+        VALUES (:username, :password_hash, :stripe_customer_id)
+        RETURNING id'
     );
     $sth->bindParam(':username', $username, PDO::PARAM_STR);
     $sth->bindParam(':password_hash', $password_hash, PDO::PARAM_STR);
     $sth->bindParam(':stripe_customer_id', $stripe_customer_id, PDO::PARAM_STR);
     $sth->execute();
+    $row = $sth->fetch(PDO::FETCH_ASSOC);
     $dbh = null;
-    return;
+    return row['id'];
 }
 
 
